7a32c47d021fbd3599727a9ebce6e496a0ee615b
tianfeng20040811-beep <tianfeng20040811@gmail.com>
2026-02-11 02:47:47 +0800

    CR-02: Add Impact (Baseline vs Optimized) comparison page with A/B baselines

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3764535571"
+    serviceWorkerVersion: "2438974714"
   }
 });
